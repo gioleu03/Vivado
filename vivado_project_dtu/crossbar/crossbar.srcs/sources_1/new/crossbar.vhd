3a45bf6bfaea67aa2322b881649dd630cca69b91
gioleu03 <giorgialeuzzi03@gmail.com>
2026-03-28 14:04:02 +0100

    start of the new project

---
+++ vivado_project_dtu/crossbar/crossbar.srcs/sources_1/new/crossbar.vhd
@@ -0,0 +1,43 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 03/28/2026 01:37:31 PM
+-- Design Name: 
+-- Module Name: crossbar - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity crossbar is
+--  Port ( );
+end crossbar;
+
+architecture Behavioral of crossbar is
+
+begin
+
+
+end Behavioral;
